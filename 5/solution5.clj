32fa07378284d2c35ef2ccc569e1161d4f9552c7
Patric Lantz <patricla@kth.se>
2020-12-05 16:45:33 +0100

    Refactor solutions of day 5

--- 5/solution5.clj
+++ 5/solution5.clj
@@ -1,47 +1,21 @@
 (ns solution5.core
   (:require [clojure.string :as str]))
 
-(defn parse-input
-  [input]
+(defn parse-input [input]
   (as-> (slurp input) $
         (str/split $ #"\n")
-        (map seq $)))
+        (map (fn [s] (-> (str/replace s #"(L|F)" "0")
+                         (str/replace   #"(R|B)" "1"))) $)))
 
-(defn search-seat
-  [low-or-high seats]
-  (let [half-seats (Math/round (double (/ (count seats) 2)))]
-    (condp = low-or-high
-      \B (drop half-seats seats)
-      \F (take half-seats seats)
-      \R (drop half-seats seats)
-      \L (take half-seats seats))))
+(defn get-seat-ids []
+  (sort (map (fn [s] (+ (Integer/parseInt (subs s 7 10) 2)
+                        (* (Integer/parseInt (subs s 0 7) 2) 8))) (parse-input "input"))))
 
-(defn get-position-from-bsp
-  [bsp row-or-column]
-  (loop [[lh seats] (condp = row-or-column
-                      :row    [(take 7 bsp) (range 0 128)]
-                      :column [(drop 7 bsp) (range 0 8)])]
-    (if (= (count lh) 0)
-      (first seats)
-      (recur [(rest lh) (search-seat (first lh) seats)]))))
+(defn get-max-seat-id []
+  (last (get-seat-ids)))
 
-(defn get-seat-id-from-bsp
-  [bsp]
-  (let [row    (get-position-from-bsp bsp :row)
-        column (get-position-from-bsp bsp :column)]
-    (+ column (* row 8))))
-
-(defn find-highest-seat-id-from-bsps
-  []
-  (->> (parse-input "input")
-       (map (fn [bsp] (get-seat-id-from-bsp bsp)))
-       (reduce (fn [acc curr] (if (> curr acc) curr acc)) 0)))
-
-(defn find-my-seat-id
-  []
-  (let [ids      (->> (parse-input "input")
-                      (map (fn [bsp] (get-seat-id-from-bsp bsp)))
-                      (sort))
+(defn find-my-seat-id []
+  (let [ids      (get-seat-ids)
         cons-ids (range (first ids) (inc (last  ids)))]
     (- (reduce - ids) (reduce - cons-ids))))
 
