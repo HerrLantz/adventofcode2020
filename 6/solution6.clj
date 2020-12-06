cb2d753e448f59a6755993d3976c722ef6b5d09f
Patric Lantz <patricla@kth.se>
2020-12-06 18:04:56 +0100

    Added second solution for day 6

--- 6/solution6.clj
+++ 6/solution6.clj
@@ -6,3 +6,5 @@
 
 (defn count-answers [] (reduce + (map (fn [a] (count (remove #(= \newline %) (set a)))) (parse-input "input"))))
 
+(defn count-answers2 [] (reduce + (map #(count (apply set/intersection (map set %))) (map #(str/split % #"\n") (parse-input "input")))))
+
