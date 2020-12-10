c1beaf7c5621f75ee70c5b5b5b36232cbbb8bdac
Patric Lantz <patricla@kth.se>
2020-12-10 02:32:55 +0100

    Added second solution for day 9

--- 9/solution9.clj
+++ 9/solution9.clj
@@ -11,16 +11,33 @@
   [number preamble]
   (loop [pre preamble]
     (cond
-      (empty? pre)                                  true
-      (some #(= number (+ % (first pre))) preamble) false
-      :else                                         (recur (rest preamble)))))
+      (empty? pre)                             true
+      (some #(= number (+ % (first pre))) pre) false
+      :else                                    (recur (rest pre)))))
 
 (defn find-non-sum
+  ([] (find-non-sum (parse-input "input")))
+  ([data]
+   (loop [preamble (take 25 data)
+          numbers  (drop 25 data)]
+     (if (number-is-non-sum? (first numbers) preamble)
+       (first numbers)
+       (recur (conj preamble (first numbers)) (rest numbers))))))
+
+(defn sub-list
+  [beginning end lst]
+  (take (- (inc end) beginning)
+        (drop beginning lst)))
+
+(defn find-contigous-set
   []
-  (as-> (parse-input "input") data
-        (loop [preamble (take 25 data)
-               numbers  (drop 25 data)]
-          (println (str "---- " (first  numbers) " ----"))
-          (if (number-is-non-sum? (first numbers) preamble)
-            (first numbers)
-            (recur (conj preamble (first numbers)) (rest numbers))))))
+  (let [data    (parse-input "input")
+        non-sum (find-non-sum data)]
+    (loop [beginning 0
+           end       0]
+      (let [sum (reduce + (sub-list beginning end data))]
+        (cond
+          (> sum non-sum) (recur (inc beginning) end)
+          (< sum non-sum) (recur beginning (inc end))
+          :else           (as-> (sort (sub-list beginning end data)) $
+                                (+ (first $) (last $))))))))
